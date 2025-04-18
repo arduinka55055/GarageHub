ac80ec4e06e028dc1abe862870769ff4a6e5e662
Denys (Linux Server) <denistesla2005@gmail.com>
2025-04-18 22:43:46 +0300

    added ipv6 only support

--- myservice/settings.py
+++ myservice/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*1olz(siuurb*vpn_eo9(y%_cvp@zt1mj_^j!e*2j)4^qby#i#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
